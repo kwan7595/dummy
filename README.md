4d9709b86bffc627c3e218222ce3b21dccc190c7
kwan <kwanhee.lee@postech.ac.kr>
2024-09-20 17:37:25 +0900

    Add readme, fix train.py

---
+++ README.md
@@ -0,0 +1,10 @@
+### Dummy codes for GAUDI HPU migration support check
+This repository contains dummy codes for checking the GAUDI HPU migration toolkit and manual model porting compatibility
+
+Will consider training exmaples of
+* MLP
+* CNN
+* Transformers
+
+
+Kwanhee Lee
--- train.py
+++ train.py
@@ -79,7 +79,7 @@ for epoch in range(3):  # loop over the dataset multiple times
     # since we're not training, we don't need to calculate the gradients for our outputs
     with torch.no_grad():
         for data in testloader:
-            images, labels = data
+            images, labels = data[0].to(device), data[1].to(device)
             # calculate outputs by running images through the network
             outputs = net(images)
             # the class with the highest energy is what we choose as prediction
